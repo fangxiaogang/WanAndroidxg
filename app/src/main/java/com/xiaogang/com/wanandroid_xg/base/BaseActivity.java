a473efdbbc77de6df95deb77b5486aeb2d184843
fangxiaogang <fangxiaogang>
2018-09-01 18:02:09 +0800

    Base commit

--- app/src/main/java/com/xiaogang/com/wanandroid_xg/base/BaseActivity.java
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/base/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.xiaogang.com.wanandroid_xg.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 import butterknife.Unbinder;
@@ -16,5 +19,24 @@ public abstract class BaseActivity <V extends BaseContract.Basepresenter> extend
     protected abstract int getLayoutid();
 
     protected abstract void initView();
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        destroyView();
+    }
+
+    private  void destroyView(){
+
+    };
+
 
 }
---
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/utils/Constant.java
@@ -0,0 +1,9 @@
+package com.xiaogang.com.wanandroid_xg.utils;
+
+/**
+ * author: fangxiaogang
+ * date: 2018/9/1
+ */
+
+public class Constant {
+}
