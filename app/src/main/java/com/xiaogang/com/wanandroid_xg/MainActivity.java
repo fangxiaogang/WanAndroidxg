406e79d03f8ee28ac7bc00bbc622efe8f1667139
fangxiaogang <fangxiaogang>
2018-09-01 12:00:51 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiaogang.com.wanandroid_xg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
