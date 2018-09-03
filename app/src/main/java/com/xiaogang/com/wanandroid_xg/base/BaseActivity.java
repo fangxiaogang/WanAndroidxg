be47618097a5967ff19595eb78a840024782d280
fangxiaogang <fangxiaogang>
2018-09-03 18:03:45 +0800

    fragmentation commit

--- app/src/main/java/com/xiaogang/com/wanandroid_xg/base/BaseActivity.java
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/base/BaseActivity.java
@@ -1,18 +1,30 @@
 package com.xiaogang.com.wanandroid_xg.base;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.xiaogang.com.wanandroid_xg.MyApplication;
+
+import javax.inject.Inject;
 
 import butterknife.Unbinder;
+import me.yokeyword.fragmentation.ExtraTransaction;
+import me.yokeyword.fragmentation.ISupportActivity;
+import me.yokeyword.fragmentation.ISupportFragment;
+import me.yokeyword.fragmentation.SupportActivityDelegate;
+import me.yokeyword.fragmentation.SupportFragment;
+import me.yokeyword.fragmentation.SupportHelper;
+import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
 /**
  * author: fangxiaogang
  * date: 2018/9/1
  */
 
-public abstract class BaseActivity <V extends BaseContract.Basepresenter> extends RxAppCompatActivity implements BaseContract.Baseview{
+public abstract class BaseActivity <V extends BaseContract.Basepresenter> extends RxAppCompatActivity implements BaseContract.Baseview,ISupportActivity{
 
     private Unbinder unbinder;
 
@@ -20,23 +32,250 @@ public abstract class BaseActivity <V extends BaseContract.Basepresenter> extend
 
     protected abstract void initView();
 
+    @Nullable
+    @Inject
+    protected V mPresenter;
 
+    protected ActivityComponent mActivityComponent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+
+    /**
+     * 初始化ActivityComponent
+     */
+    private void initActivityComponent() {
+        mActivityComponent = DaggerActivityComponent.builder()
+                .applicationComponent(((MyApplication) getApplication()).getApplicationComponent())
+                .activityModule(new ActivityModule(this))
+                .build();
+    }
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        mDelegate.onDestroy();
+        unbinder.unbind();
         destroyView();
     }
 
-    private  void destroyView(){
 
-    };
+    /**
+     * 贴上view
+     */
+    private void attachView() {
+        if (mPresenter != null) {
+            mPresenter.attachView(this);
+        }
+    }
+
+    /**
+     * 分离view
+     */
+    private void destroyView() {
+        if (mPresenter != null) {
+            mPresenter.detachView();
+        }
+    }
+
+
+
+
+
+    final SupportActivityDelegate mDelegate = new SupportActivityDelegate(this);
+
+    @Override
+    public SupportActivityDelegate getSupportDelegate() {
+        return mDelegate;
+    }
+
+    /**
+     * Perform some extra transactions.
+     * 额外的事务：自定义Tag，添加SharedElement动画，操作非回退栈Fragment
+     */
+    @Override
+    public ExtraTransaction extraTransaction() {
+        return mDelegate.extraTransaction();
+    }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDelegate.onPostCreate(savedInstanceState);
+    }
+
+
+    /**
+     * Note： return mDelegate.dispatchTouchEvent(ev) || super.dispatchTouchEvent(ev);
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return mDelegate.dispatchTouchEvent(ev) || super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 不建议复写该方法,请使用 {@link #onBackPressedSupport} 代替
+     */
+    @Override
+    final public void onBackPressed() {
+        mDelegate.onBackPressed();
+    }
+
+    /**
+     * 该方法回调时机为,Activity回退栈内Fragment的数量 小于等于1 时,默认finish Activity
+     * 请尽量复写该方法,避免复写onBackPress(),以保证SupportFragment内的onBackPressedSupport()回退事件正常执行
+     */
+    @Override
+    public void onBackPressedSupport() {
+        mDelegate.onBackPressedSupport();
+    }
+
+    /**
+     * 获取设置的全局动画 copy
+     *
+     * @return FragmentAnimator
+     */
+    @Override
+    public FragmentAnimator getFragmentAnimator() {
+        return mDelegate.getFragmentAnimator();
+    }
+
+    /**
+     * Set all fragments animation.
+     * 设置Fragment内的全局动画
+     */
+    @Override
+    public void setFragmentAnimator(FragmentAnimator fragmentAnimator) {
+        mDelegate.setFragmentAnimator(fragmentAnimator);
+    }
+
+    /**
+     * Set all fragments animation.
+     * 构建Fragment转场动画
+     * <p/>
+     * 如果是在Activity内实现,则构建的是Activity内所有Fragment的转场动画,
+     * 如果是在Fragment内实现,则构建的是该Fragment的转场动画,此时优先级 > Activity的onCreateFragmentAnimator()
+     *
+     * @return FragmentAnimator对象
+     */
+    @Override
+    public FragmentAnimator onCreateFragmentAnimator() {
+        return mDelegate.onCreateFragmentAnimator();
+    }
+
+    /**
+     * Causes the Runnable r to be added to the action queue.
+     * <p>
+     * The runnable will be run after all the previous action has been run.
+     * <p>
+     * 前面的事务全部执行后 执行该Action
+     */
+    @Override
+    public void post(Runnable runnable) {
+        mDelegate.post(runnable);
+    }
+
+
+    // 选择性拓展其他方法
+
+    public void loadRootFragment(int containerId, @NonNull ISupportFragment toFragment) {
+        mDelegate.loadRootFragment(containerId, toFragment);
+    }
+
+    public void start(ISupportFragment toFragment) {
+        mDelegate.start(toFragment);
+    }
+
+    /**
+     * @param launchMode Same as Activity's LaunchMode.
+     */
+    public void start(ISupportFragment toFragment, @ISupportFragment.LaunchMode int launchMode) {
+        mDelegate.start(toFragment, launchMode);
+    }
+
+    /**
+     * It is recommended to use {@link SupportFragment#startWithPopTo(ISupportFragment, Class, boolean)}.
+     *
+     * @see #popTo(Class, boolean)
+     * +
+     * @see #start(ISupportFragment)
+     */
+    public void startWithPopTo(ISupportFragment toFragment, Class<?> targetFragmentClass, boolean includeTargetFragment) {
+        mDelegate.startWithPopTo(toFragment, targetFragmentClass, includeTargetFragment);
+    }
+
+    /**
+     * Pop the fragment.
+     */
+    public void pop() {
+        mDelegate.pop();
+    }
+
+    /**
+     * Pop the last fragment transition from the manager's fragment
+     * back stack.
+     */
+    public void popTo(Class<?> targetFragmentClass, boolean includeTargetFragment) {
+        mDelegate.popTo(targetFragmentClass, includeTargetFragment);
+    }
+
+    /**
+     * If you want to begin another FragmentTransaction immediately after popTo(), use this method.
+     * 如果你想在出栈后, 立刻进行FragmentTransaction操作，请使用该方法
+     */
+    public void popTo(Class<?> targetFragmentClass, boolean includeTargetFragment, Runnable afterPopTransactionRunnable) {
+        mDelegate.popTo(targetFragmentClass, includeTargetFragment, afterPopTransactionRunnable);
+    }
+
+    public void popTo(Class<?> targetFragmentClass, boolean includeTargetFragment, Runnable afterPopTransactionRunnable, int popAnim) {
+        mDelegate.popTo(targetFragmentClass, includeTargetFragment, afterPopTransactionRunnable, popAnim);
+    }
+
+    /**
+     * 得到位于栈顶Fragment
+     */
+    public ISupportFragment getTopFragment() {
+        return SupportHelper.getTopFragment(getSupportFragmentManager());
+    }
+
+    /**
+     * 获取栈内的fragment对象
+     */
+    public <T extends ISupportFragment> T findFragment(Class<T> fragmentClass) {
+        return SupportHelper.findFragment(getSupportFragmentManager(), fragmentClass);
+    }
+
+    /**
+     * 加载多个同级根Fragment,类似Wechat, QQ主页的场景
+     */
+    public void loadMultipleRootFragment(int containerId, int showPosition, ISupportFragment... toFragments) {
+        mDelegate.loadMultipleRootFragment(containerId, showPosition, toFragments);
+    }
+
+    /**
+     * show一个Fragment,hide其他同栈所有Fragment
+     * 使用该方法时，要确保同级栈内无多余的Fragment,(只有通过loadMultipleRootFragment()载入的Fragment)
+     * <p>
+     * 建议使用更明确的{@link #showHideFragment(ISupportFragment, ISupportFragment)}
+     *
+     * @param showFragment 需要show的Fragment
+     */
+    public void showHideFragment(ISupportFragment showFragment) {
+        mDelegate.showHideFragment(showFragment);
+    }
+
+    /**
+     * show一个Fragment,hide一个Fragment ; 主要用于类似微信主页那种 切换tab的情况
+     */
+    public void showHideFragment(ISupportFragment showFragment, ISupportFragment hideFragment) {
+        mDelegate.showHideFragment(showFragment, hideFragment);
+    }
+
 
 
 }
