ecf71e879081d86b274254c7080878ca3544dc4e
fangxiaogang <1005275062@qq.com>
2018-09-06 23:41:57 +0800

    login

---
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/login/LoginContract.java
@@ -0,0 +1,22 @@
+package com.xiaogang.com.wanandroid_xg.ui.login;
+
+import com.xiaogang.com.wanandroid_xg.base.BaseContract;
+import com.xiaogang.com.wanandroid_xg.bean.User;
+
+/**
+ * Created by xiaogang on 2018/9/6.
+ */
+
+public interface LoginContract {
+
+    interface View extends BaseContract.Baseview {
+        void loginSuccess(User user);
+
+        void loginerror(String error);
+    }
+
+    interface Presenter extends BaseContract.Basepresenter<LoginContract.View> {
+        void login(String username, String password);
+    }
+
+}
