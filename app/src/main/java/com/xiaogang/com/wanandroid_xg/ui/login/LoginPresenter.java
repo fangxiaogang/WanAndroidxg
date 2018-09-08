672609b1af1d625eb73ad6298e49f992e9c5e707
fangxiaogang <1005275062@qq.com>
2018-09-08 23:02:03 +0800

    login

---
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/login/LoginPresenter.java
@@ -0,0 +1,22 @@
+package com.xiaogang.com.wanandroid_xg.ui.login;
+
+import com.xiaogang.com.wanandroid_xg.base.BasePresenter;
+
+import javax.inject.Inject;
+
+/**
+ * Created by xiaogang on 2018/9/8.
+ */
+
+public class LoginPresenter extends BasePresenter<LoginContract.View> implements LoginContract.Presenter {
+
+    @Inject
+    public LoginPresenter() {
+
+    }
+
+    @Override
+    public void login(String username, String password) {
+
+    }
+}
