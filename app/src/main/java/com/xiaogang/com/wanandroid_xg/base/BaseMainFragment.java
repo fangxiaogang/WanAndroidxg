015c777d55bd0f407a3df6b16cba9122ec91535d
fangxiaogang <1005275062@qq.com>
2018-09-22 22:36:52 +0800

    MainFragment

--- app/src/main/java/com/xiaogang/com/wanandroid_xg/base/BaseMainFragment.java
+++
@@ -1,8 +0,0 @@
-package com.xiaogang.com.wanandroid_xg.base;
-
-/**
- * author: fangxiaogang
- * date: 2018/9/21
- */
-
-
---
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/main/MainFragment.java
@@ -0,0 +1,9 @@
+package com.xiaogang.com.wanandroid_xg.ui.main;
+
+/**
+ * Created by xiaogang on 2018/9/22.
+ */
+
+public class MainFragment  {
+
+}
