9450a90278f6c3a038dd499edd79705f9b9b2570
fangxiaogang <1005275062@qq.com>
2018-10-11 18:48:13 +0800

    aboutFragment

--- app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/mine/AboutFragment.java
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/mine/AboutFragment.java
@@ -1,10 +1,17 @@
 package com.xiaogang.com.wanandroid_xg.ui.mine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.xiaogang.com.wanandroid_xg.R;
 import com.xiaogang.com.wanandroid_xg.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * author: fangxiaogang
@@ -12,6 +19,22 @@ import com.xiaogang.com.wanandroid_xg.base.BaseFragment;
  */
 
 public class AboutFragment extends BaseFragment {
+
+    @BindView(R.id.backtitle)
+    ImageView backtitle;
+
+    @BindView(R.id.homesearch)
+    ImageView mhomesearch;
+
+    @BindView(R.id.starlin)
+    LinearLayout mstarlin;
+
+    @BindView(R.id.upgradelin)
+    LinearLayout mupgradelin;
+
+    @BindView(R.id.githunlin)
+    LinearLayout mgithunlin;
+
 
     @Override
     protected int getLayoutId() {
@@ -25,6 +48,39 @@ public class AboutFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
+
+        backtitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _mActivity.onBackPressed();
+            }
+        });
+
+        mupgradelin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.showShort("To do");
+            }
+        });
+
+        mstarlin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.intent.action.VIEW");
+                intent.setData(Uri.parse("https://github.com/fangxiaogang/WanAndroidxg"));
+                startActivity(intent);
+            }
+        });
+
+        mgithunlin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.intent.action.VIEW");
+                intent.setData(Uri.parse("https://github.com/fangxiaogang"));
+                startActivity(intent);
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/mine/MineFragment.java
+++ app/src/main/java/com/xiaogang/com/wanandroid_xg/ui/mine/MineFragment.java
@@ -3,6 +3,7 @@ package com.xiaogang.com.wanandroid_xg.ui.mine;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ import com.xiaogang.com.wanandroid_xg.net.CookiesManager;
 import com.xiaogang.com.wanandroid_xg.ui.login.LoginFragment;
 import com.xiaogang.com.wanandroid_xg.ui.main.MainFragment;
 import com.xiaogang.com.wanandroid_xg.ui.mycollect.MyCollectFragment;
+import com.xiaogang.com.wanandroid_xg.ui.search.SearchFragment;
 import com.xiaogang.com.wanandroid_xg.utils.Constant;
 
 import java.util.List;
@@ -34,11 +36,11 @@ public class MineFragment extends BaseFragment {
     @BindView(R.id.out)
     LinearLayout outlin;
 
-    @BindView(R.id.test1)
-    Button test1;
+    @BindView(R.id.aboutlin)
+    LinearLayout maboutlin;
 
-    @BindView(R.id.test2)
-    Button test2;
+    @BindView(R.id.homesearch)
+    ImageView mhomesearch;
 
     @Override
     protected int getLayoutId() {
@@ -52,6 +54,13 @@ public class MineFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
+
+        mhomesearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainFragment) getParentFragment()).startBrotherFragment(SearchFragment.newInstance());
+            }
+        });
 
         loginlin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,23 +79,11 @@ public class MineFragment extends BaseFragment {
             }
         });
 
-        test1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SPUtils.getInstance(Constant.SPname).clear();
-                CookiesManager.clearAllCookies();
-            }
-        });
 
-        test2.setOnClickListener(new View.OnClickListener() {
+        maboutlin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Cookie> cookies = CookiesManager.getCookies();
-               if (CookiesManager.getCookies().size() == 0) {
-                   ToastUtils.showShort("cookie null");
-               } else {
-                   ToastUtils.showShort("cookie not null");
-               }
+                ((MainFragment) getParentFragment()).startBrotherFragment(AboutFragment.newInstance());
             }
         });
     }
